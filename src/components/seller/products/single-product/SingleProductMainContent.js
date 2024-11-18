84775435542819c1976c917ac67bf4fb9c355f99
Dominic Lakshan <dominiclakshank@gmail.com>
2024-11-18 23:40:19 +0530

    feat: add ProductDetails component and integrate it into SingleProductMainContent

--- src/components/seller/products/single-product/SingleProductMainContent.js
+++ src/components/seller/products/single-product/SingleProductMainContent.js
@@ -1,13 +1,16 @@
-import { Button, Col, Flex, Row } from "antd";
+import { Col, Row } from "antd";
 import React from "react";
 import ProductImageCarousel from "./ProductImageCarousel";
-import { DownloadOutlined } from "@ant-design/icons";
+import ProductDetails from "./ProductDetails";
 
 const SingleProductMainContent = ({ product }) => {
   return (
     <Row gutter={[16, 16]} justify="start">
       <Col xs={24} sm={24} md={12} lg={12} xl={8}>
         <ProductImageCarousel images={product} />
+      </Col>
+      <Col xs={24} sm={24} md={12} lg={12} xl={16}>
+        <ProductDetails product={product} />
       </Col>
     </Row>
   );
