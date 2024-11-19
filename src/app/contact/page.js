9d880f5779c9f7fa88a73257a5101672bd2bce4f
Dominic Lakshan <dominiclakshank@gmail.com>
2024-11-19 19:31:56 +0530

    feat: add About Us and Contact Us pages, enhance Navbar for routing, and update HomeCarousel styles

---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+import HomeLayout from "@/components/home/HomeLayout";
+import React from "react";
+
+const ContactUsPage = () => {
+  return <HomeLayout>Contact Us</HomeLayout>;
+};
+
+export default ContactUsPage;
